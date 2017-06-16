0a9bfaf0c09388b0891ad21940de02f31fac7353
masihcs <masihkf@hi2.in>
2017-06-17 01:09:52 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -19,7 +19,7 @@ local notify = lgi.require('Notify')
 notify.init ("Telegram updates")
 chats = {}
 plugins = {}
-helper_id = 340155871 --Put Your Helper Bot ID Here
+helper_id = 398312272 --Put Your Helper Bot ID Here
 
 function do_notify (user, msg)
 	local n = notify.Notification.new(user, msg)
@@ -90,19 +90,17 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {434741789, 398312272, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v6.0
+    info_text = [[》cyber storm v1
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》https://github.com/masihcs/antis
 
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
+》@masihking79 ➣ Founder & Developer《
 
 》Special thanks to :
 》Beyond Team Members
@@ -111,7 +109,7 @@ An advanced administration bot based on https://valtman.name/telegram-cli
 》@BeyondTeam《
 
 》Our website :
-》http://BeyondTeam.ir
+》....
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
